2e617a6dd42ccd6e0659c19788d91e3e67a18f2f
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2013-04-09 22:16:11 +0530

    pos and users atk folders are now submodules of the project

--- pos/atk4-addons/misc/lib/Export.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* to enable compatbility mode:
- * redefine class Export extends Export_Compat {}
- */
-namespace misc;
-class Export extends Export_Advanced {}
--- pos/atk4-addons/misc/lib/Form/Field/drilldown.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace misc;
-
-// Left here for backward compatibilidty. Moved to hierarchy add-on.
-class Form_Field_drilldown extends \hierarchy\Form_Field_drilldown {}
